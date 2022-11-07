65e21a6249a434271b9a88698078307fd4cebc2c
Erodri6z <erikrodriguez2417@gmail.com>
2022-11-07 17:35:00 -0600

    stub up create function on notes services

--- src/services/notesService.js
+++ src/services/notesService.js
@@ -2,7 +2,19 @@ import * as tokenService from './tokenService'
 
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/notes`
 
+async function create(note) {
+  const res = await fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(note)
+  })
+  return await res.json()
+}
+
 
 export {
-  
+  create
 }
