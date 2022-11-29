e825c0eedc94bb15ce2588067fc408076bb4810d
Erodri6z <erikrodriguez2417@gmail.com>
2022-11-28 21:01:38 -0600

    clean up nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,11 +9,10 @@ const NavBar = ({ user, handleLogout }) => {
         <nav>
           <ul>
             <li id='username'>Welcome, {user.name}</li>
-            <li><Link className='nav-item' to="/profiles">Profiles</Link></li>
             <li><Link className='nav-item' to="/new-entry">Vent</Link></li>
             <li><Link className='nav-item' to="/journal">Journal</Link></li>
             <li><Link className='nav-item' to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link className='nav-item' to="/changePassword">Change Password</Link></li>
+            {/* <li><Link className='nav-item' to="/changePassword">Change Password</Link></li> */}
           </ul>
         </nav>
       :
